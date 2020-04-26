20effb0735e390bcb44c7650bbe9177772e5e6fe
Nithyasree <54515763+Nithy-Sree@users.noreply.github.com>
2020-04-26 10:40:38 +0530

    JSPrograms

---
+++ fact.js
@@ -0,0 +1,7 @@
+let a=5;
+var f=1;
+for(let i=1;i<=a;i++)
+{
+    f=f*i;
+}
+console.log(f);
---
+++ fibonacci.js
@@ -0,0 +1,10 @@
+var n=7;
+var a=-1,b=1;
+var c;
+for(let i=0;i<=n;i++)
+{
+    c=a+b;
+    console.log(c);
+    a=b;
+    b=c;
+}
